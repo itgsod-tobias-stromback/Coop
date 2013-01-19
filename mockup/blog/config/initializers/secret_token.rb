b887b4bc4b215730e77f261c7ce98a962aa0fd49
Tobias Strömbäck <Tobias.HultingStromback@elev.it-gymnasiet.se>
2013-01-19 22:43:01 +0100

    deleted old mockup

--- mockup/blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '8d4f8815dd40f303278d457fe2f8691bf29156ca2ec883c7e58ed925e1a084e936457a1fe15851fb96eb8196499deacb3e7aca463ae9e0ecdee9279378bb9dbb'
