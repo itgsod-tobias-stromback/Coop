535af50b79bb179cb6a4dbbe2772cf090d41d61e
Tobias Hulting Strömbäck <tobias.hultingstromback@elev.it-gymnasiet.se>
2013-04-09 12:15:03 +0200

    encoding samt config redigerat/tillagt

--- testsida2/app/controllers/stuffs_controller.rb
+++ testsida2/app/controllers/stuffs_controller.rb
@@ -1,4 +1,7 @@
+# encoding: UTF-8
 class StuffsController < ApplicationController
   active_scaffold :stuff do |conf|
+  	conf.create.link.label = "Lägg till ny"
+  	conf.search.link.label = "Sök"
   end
 end 
--- testsida2/app/models/stuff.rb
+++ testsida2/app/models/stuff.rb
@@ -1,3 +1,6 @@
 class Stuff < ActiveRecord::Base
+	def to_label
+		": #{Stuff}"
+	end
   attr_accessible :datum, :int1, :int2, :int3, :string1, :string2, :string3
 end
