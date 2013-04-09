4d5e11f38359d2abd63e85d20a0ee28f65128d7b
Tobias Hulting Strömbäck <tobias.hultingstromback@elev.it-gymnasiet.se>
2013-04-09 13:02:55 +0200

    css + js fixat

--- coopsida/app/assets/javascripts/application.js
+++ coopsida/app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery-ui
 //= require jquery_ujs
+//= require active_scaffold
 //= require_tree .
